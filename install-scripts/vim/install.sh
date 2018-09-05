0ee5e5b874d7585a5b9f621a489fa44becdf1f71
Brandon Dees <brandon@rietta.com>
2018-09-05 02:25:16 -0500

    Don't just install vim-plug, install the plugins too

--- install-scripts/vim/install.sh
+++ install-scripts/vim/install.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+
 sudo apt install -y vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PluginInstall +qall
