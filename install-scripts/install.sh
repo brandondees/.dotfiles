68d0a674c4678f861bd0a585638eb3a266c920e0
Brandon Dees <brandon@rietta.com>
2018-09-18 16:01:04 -0500

    Update and organize install scripts list

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -38,16 +38,18 @@ if [ ! -d ~/.dotfiles ]; then
   git clone git@github.com:brandondees/.dotfiles.git ~/.dotfiles || git clone https://github.com/brandondees/.dotfiles ~/.dotfiles
 fi
 
-$HOME/.dotfiles/install-scripts/vim/install.sh
+$HOME/.dotfiles/install-scripts/asdf/install.sh
+$HOME/.dotfiles/install-scripts/autojump/install.sh
 $HOME/.dotfiles/install-scripts/google-chrome/install.sh
-$HOME/.dotfiles/install-scripts/zsh/install.sh
+$HOME/.dotfiles/install-scripts/maria-db/install.sh
+$HOME/.dotfiles/install-scripts/postgresql/install.sh
 $HOME/.dotfiles/install-scripts/redis/install.sh
 $HOME/.dotfiles/install-scripts/thefuck/install.sh
-$HOME/.dotfiles/install-scripts/asdf/install.sh
-# $HOME/.dotfiles/install-scripts/bc3/install.sh
-$HOME/.dotfiles/install-scripts/postgresql/install.sh
-$HOME/.dotfiles/install-scripts/maria-db/install.sh
-$HOME/.dotfiles/install-scripts/autojump/install.sh
+$HOME/.dotfiles/install-scripts/tig/install.sh
+$HOME/.dotfiles/install-scripts/tldr/install.sh
+$HOME/.dotfiles/install-scripts/tmuxinator/install.sh
+$HOME/.dotfiles/install-scripts/vim/install.sh
+$HOME/.dotfiles/install-scripts/zsh/install.sh
 
 # load in asdf for Ruby that was only JUST installed above
 . $HOME/.asdf/asdf.sh
