4be1fa7f32a81da4a356294855b357075f18fe8b
Brandon Dees <brandon@rietta.com>
2018-09-18 15:07:45 -0500

    Add install script to help set up audio devices better

---
+++ install-scripts/audio/install.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+sudo apt update
+sudo apt install -y pulseaudio pavucontrol
+
+echo "Installed pavucontrol to enable more control over audio devices. No specific configuration added yet."
+
