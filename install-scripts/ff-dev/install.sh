31c83fc78430a157589e9351b9a1ca6c243abde3
Brandon Dees <dees@pm.me>
2019-04-11 22:39:14 -0500

    Install for firefox developer edition, missing an app menu shortcut and profile management

---
+++ install-scripts/ff-dev/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+sudo add-apt-repository ppa:mozillateam/firefox-next
+sudo apt-get update
+sudo apt-get install firefox
