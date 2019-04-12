5c4003af77f7139a695777f482a3ac991dfa8bc3
Brandon Dees <dees@pm.me>
2019-04-12 14:31:06 -0500

    Use adr-tools to implement documented architecture decisons

--- install-scripts/asdf/install.sh
+++ install-scripts/asdf/install.sh
@@ -1,14 +1,22 @@
 #! /usr/bin/env bash
 if [ ! -d ~/.asdf ]; then
-  git clone git@github.com:asdf-vm/asdf.git ~/.asdf --branch v0.3.0 || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+  git clone git@github.com:asdf-vm/asdf.git ~/.asdf --branch v0.7.1 \
+    || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
 fi
 
 . $HOME/.asdf/asdf.sh
 
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf update
+asdf plugin-add adr-tools
+asdf plugin-add ruby
+asdf plugin-add nodejs
+asdf plugin-add
+asdf plugin-update --all
 
 asdf install ruby 2.5.1
 asdf global ruby 2.5.1
+
+asdf install adr-tools 3.0.0
+asdf global adr-tools 3.0.0
 
 cat $HOME/.dotfiles/.default-gems | xargs gem install
