a9d8a3b95abbc640bf92cf5020af34f6c7cb4bc1
Brandon Dees <dees@pm.me>
2019-04-12 19:17:15 -0500

    Fix brightness control for repo changes

--- install-scripts/display-brightness/install.sh
+++ install-scripts/display-brightness/install.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 # My goal with this is to have a general purpose utility to manage display brightness.
-# I'm specifically working with a 2014 Apple Thunderbolt display 
+# I'm specifically working with a 2014 Apple Thunderbolt display
 # which seems to be unsupported by OS default device management UI.
-# 
+#
 # This is far from 100% but at least is starting progress towards a solution.
 
 # Background reference:
@@ -21,3 +21,8 @@ ddcutil environment
 # I've also made progress with https://github.com/warvariuc/acdcontrol
 # and it can be run without sudo by using:
 # `sudo setcap cap_dac_override+ep path/to/acdcontrol`
+git clone https://github.com/warvariuc/acdcontrol.git ~/acdcontrol
+cd ~/acdcontrol
+make
+
+sudo setcap cap_dac_override+ep ~/acdcontrol/acdcontrol
