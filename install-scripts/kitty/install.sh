512dfc47bcd78ced5d66762c6a04f59eb5f0b645
Brandon Dees <dees@pm.me>
2019-04-13 00:29:55 -0500

    Fix kitty bin link

--- install-scripts/kitty/install.sh
+++ install-scripts/kitty/install.sh
@@ -8,7 +8,7 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 # create shortcuts
 # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
 # your PATH)
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+ln -s ~/.local/kitty.app/bin/kitty ~/bin/
 # Place the kitty.desktop file somewhere it can be found by the OS
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 # Update the path to the kitty icon in the kitty.desktop file
