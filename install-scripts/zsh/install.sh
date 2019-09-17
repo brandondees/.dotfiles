02155da5a364111ff07f0be5ae3c45dbbfa9534d
Brandon Dees <dees@pm.me>
2019-09-16 23:35:11 -0500

    tweaks

--- install-scripts/zsh/install.sh
+++ install-scripts/zsh/install.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
-sudo apt install -y zsh
+# sudo apt install -y zsh
 if [ ! -d ~/.oh-my-zsh ]; then
   git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 sudo chsh -s /bin/zsh
+chsh -s /bin/zsh
 
