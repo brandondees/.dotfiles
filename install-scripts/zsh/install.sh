a15612e7aa5f358209f98e3f3b6e1be04dfa4849
Brandon Dees <dees@pm.me>
2019-09-19 16:21:12 -0500

    zsh autocompletion

--- install-scripts/zsh/install.sh
+++ install-scripts/zsh/install.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
-# sudo apt install -y zsh
+
+install-package zsh
+
 if [ ! -d ~/.oh-my-zsh ]; then
   git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 sudo chsh -s /bin/zsh
 chsh -s /bin/zsh
-
